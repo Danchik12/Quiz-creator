36a6e8f2338dd36bef1cd3561c621950c33b1d49
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-02-17 06:53:29 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React  from 'react'
+import Layout from "./components/Layot/Layout"
+import Quiz from "./components/Quiz/Quiz"
+function App() {
+  return (
+    <Layout>
+<Quiz/>
+
+
+
+
+    </Layout>
+  );
+}
+
+export default App;
