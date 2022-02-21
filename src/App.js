d173bae4178ac824864ac9cd2311983ef403e96b
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-02-21 18:38:45 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React  from 'react'
 import Layout from "./components/Layot/Layout"
+import {Route,Routes} from 'react-router-dom'
 import Quiz from "./components/Quiz/Quiz"
+import Auth from "./components/Auth/Auth"
+import QuizCreator from "./components/Quiz-creator/QuizCreator"
+import Quizes from "./components/Quizes/Quizes"
+
+
 function App() {
   return (
     <Layout>
-<Quiz/>
+
+ <Routes>
+      <Route path='/' element={<Quizes/>}/>
+      <Route path='/auth' element={<Auth/>}/>
+      <Route path='/quiz/:id' element={<Quiz/>}/>
+      <Route path='/create-quiz' element={<QuizCreator/>}/>
+        </Routes>
+      
 
 
-
-
-    </Layout>
+</Layout>
   );
 }
 
