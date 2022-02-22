2f4d1849f9947ed22c26b273b084e479be28fec2
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-02-22 21:17:50 +0300

    add Auth,view password styles Auth

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,13 +1,64 @@
 import './Auth.css'
 import React,{Component} from 'react'
-
-
+import {RiLockPasswordLine} from "react-icons/ri"
+import Button from './../UI/Button/Button';
 export default class Auth extends Component{
-	render(){
-		return(
-<div>
-<h1>Авторизация</h1>
+	state ={
+		close:false
+	}
+loginHandler=()=>{
 
+}
+registerHandler =()=>{
+
+}
+submitHand =event =>{
+event.preventDefault()
+}
+
+ show_hide_password =() =>{
+	var input = document.getElementById('password-input');
+	if (input.getAttribute('type') == 'password') {
+		
+		input.setAttribute('type', 'text');
+		this.setState({
+			close:!this.state.close
+		})
+	} else {
+		
+		input.setAttribute('type', 'password');
+		this.setState({
+			close:false
+		})
+	}
+	return false;
+}
+
+render (){
+		return(
+<div className="Auth">
+<div><h1>Авторизация</h1>
+
+<form onSubmit={this.submitHand} className="AuthForm">
+     <label for="login">Login</label>
+      <input  type="text" id='login' required/>
+      
+      <label for="password-input">Password</label>
+      <div className='pass'>
+      <input  type="password" id='password-input' required />
+      
+      { this.state.close ?
+      <RiLockPasswordLine className='control' onClick={this.show_hide_password}/> :
+      <RiLockPasswordLine className='no' onClick={this.show_hide_password} />
+   }
+    </div>
+      
+      <Button type="sucess" onClick={this.loginHandler}>Войти</Button>
+      <Button type="primary" onClick={this.registerHandler}>Зарегистрироваться</Button>
+    </form>
+
+
+</div>
 </div>
 
 
