9b43b86b141341ec669e0523776f31345ceba22b
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-02-28 11:29:35 +0300

    redux in QuizCreator

---
+++ store/reducer/create.js
@@ -0,0 +1,23 @@
+import {CREATE_QUESTION,RESET_QUIZ_CREATION} from './../action/actionTypes'
+const initialState = {
+	quiz : [],
+	quizName:''
+}
+
+
+export default function createReducer(state=initialState,action){
+	switch(action.type){
+		case CREATE_QUESTION:
+			return{
+				...state,
+				quiz:[...state.quiz,action.item],
+				quizName:action.quizName
+			}
+		case RESET_QUIZ_CREATION:
+		return {
+			...state,quiz:[],quizName:''
+		}
+		default:
+		return state
+	}
+}
